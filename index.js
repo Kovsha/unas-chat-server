101652135305fba0e6a3958cefc731ca0cb9750d
Kovsha <lkosa@muzixgroup.com>
2025-12-09 12:25:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
+// Statikus fájlok kiszolgálása (pl. chat kliens HTML)
+app.use(express.static('public'));
+
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
-  socket.on('chat_message', (data) => io.emit('chat_message', data));
+
+  socket.on('chat_message', (data) => {
+    console.log('Message:', data);
+    io.emit('chat_message', data);
+  });
+
   socket.on('disconnect', () => console.log('User disconnected'));
 });
 
